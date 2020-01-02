e2bfe3ee73d6b6db8c49394c93d9028729cde7f5
Kateb <katebg@yahoo.com>
2020-01-02 12:35:45 +0100

    first update in 2020

--- typings/cordova-typings.d.ts
+++ typings/cordova-typings.d.ts
@@ -1,2 +1,6 @@
-
 /// <reference path="..\.vscode\typings\cordova\cordova.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Device.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\InAppBrowser.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Vibration.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\StatusBar.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Dialogs.d.ts"/>
